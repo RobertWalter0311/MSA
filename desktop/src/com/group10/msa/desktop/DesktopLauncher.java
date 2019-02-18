8f6bab03c5de3e146c8452524bbb346877e3a2f4
robertwalter0311 <robertwalter0311@gmail.com>
2019-02-18 12:51:41 +0100

    Initial commit

---
+++ desktop/src/com/group10/msa/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.group10.msa.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.group10.msa.MainHere;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainHere(), config);
+	}
+}
