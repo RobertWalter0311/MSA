3d50368f616e683906db52fc7326c7e58f752024
robertwalter0311 <robertwalter0311@gmail.com>
2019-02-19 14:15:45 +0100

    Test

--- desktop/src/com/group10/msa/desktop/DesktopLauncher.java
+++ desktop/src/com/group10/msa/desktop/DesktopLauncher.java
@@ -5,8 +5,12 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.group10.msa.MainHere;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MainHere(), config);
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "hello-world";
+		cfg.width = 480;
+		cfg.height = 320;
+
+		new LwjglApplication(new MainHere(), cfg);
 	}
 }
